144c637a24a577a567260a819a5cfd2310c6aa78
Nick Stebb <nickolarse@riseup.net>
2022-08-04 14:27:30 +1200

    remove some mess around code from scaffold

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Cat;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,8 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/cats/{name}', function ($name) {
-    return view('cats', Cat::getAll());
 });
